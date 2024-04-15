c3bd513694fec0bf7e3501f3d2ff78f630d6e898
Areeb Ayubi <sample@madeup.com>
2024-04-15 21:02:06 +0100

    Moved get_db from main.py to database.py

--- app/main.py
+++ app/main.py
@@ -8,20 +8,12 @@ from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 from app import models
-from app.database import engine, SessionLocal
+from app.database import engine, get_db
 
 
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 
 class Post(BaseModel):
