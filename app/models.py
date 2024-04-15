c0112db12566592b46018815f583d3569a6d8794
Areeb Ayubi <sample@madeup.com>
2024-04-15 19:46:08 +0100

    Added prototype Post table
    Contains the following fields: id, title, content, published

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String, TIMESTAMP
+from app.database import Base
+
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, default=True)
+    #created_at = Column(TIMESTAMP, nullable=False)
