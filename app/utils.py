1b376abd43a9aa0dc94a25619d304df39e0d4d71
Areeb Ayubi <sample@madeup.com>
2024-04-16 15:39:07 +0100

    Added verify function in utils.py

--- app/utils.py
+++ app/utils.py
@@ -3,5 +3,12 @@ from passlib.context import CryptContext
 
 PWD_CONTEXT = CryptContext(schemes="bcrypt", deprecated="auto")
 
-def hash(password: str):
+def hash(password: str) -> str:
     return PWD_CONTEXT.hash(password)
+
+
+def verify(
+        plain_password:str,
+        hashed_password:str
+        ) -> bool:
+    return PWD_CONTEXT.verify(plain_password, hashed_password)
