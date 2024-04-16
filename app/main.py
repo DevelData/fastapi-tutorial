ca82113131d1ac8170a384484305883289a7ae7b
Areeb Ayubi <sample@madeup.com>
2024-04-16 14:30:59 +0100

    Added UserOut to response_model in decorator of get_user

--- app/main.py
+++ app/main.py
@@ -173,7 +173,7 @@ def create_user(user:UserCreate, db:Session=Depends(get_db)):
     return new_user
 
 
-@app.get("/users/{id}")
+@app.get("/users/{id}", response_model=UserOut)
 def get_user(id:int, db:Session=Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     print(user)
