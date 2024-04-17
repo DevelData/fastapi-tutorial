b10a5be2f8418ec381456cc3a3adfbf952ada684
Areeb Ayubi <sample@madeup.com>
2024-04-17 14:54:21 +0100

    Added get_current_user function in oauth2.py

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timedelta
 from typing import Dict
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from app.schemas import TokenData
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # Not a good idea to store these in the code
 SECRET_KEY = "bbS4x5rGh5H5oU9R1ZCC4TndPDtZxj2tuJayybFpKzPqKgErjJPZm832K7Qsan2t"
@@ -38,3 +43,13 @@ def verify_access_token(
         raise credentials_exception
     
     return token_data
+
+
+def get_current_user(token:str=Depends()):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate:": "Bearer"} # What is this?
+        )
+
+    return verify_access_token(token, credentials_exception)
