c04c21a909a51b2a7a86d831bf3dcfb04f007065
Areeb Ayubi <sample@madeup.com>
2024-04-18 08:01:07 +0100

    Added owner_id attribute in Post class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Boolean, Column, Integer, String, TIMESTAMP
+from sqlalchemy import (
+    Boolean, Column, 
+    Integer, String, 
+    TIMESTAMP, ForeignKey)
 from sqlalchemy.sql.expression import text
 from app.database import Base
 
@@ -11,7 +14,11 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="true", nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        server_default=text("now()"), nullable=False)
+    owner_id = Column(Integer,
+                      ForeignKey("users.id", ondelete="CASCADE"),
+                      nullable=False)
 
 
 
@@ -21,4 +28,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        server_default=text("now()"), nullable=False)
