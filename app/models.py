ffd665953ed93ea1731bca60b2bd8808cee5ed8d
Areeb Ayubi <sample@madeup.com>
2024-04-18 07:55:19 +0100

    Formatting changes

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from app.database import Base
 
 class Post(Base):
     __tablename__ = "posts"
+
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
@@ -16,6 +17,7 @@ class Post(Base):
 
 class User(Base):
     __tablename__ = "users"
+    
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
