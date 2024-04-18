dbc889486027390ae24dcb6ac47db79f1079e864
Areeb Ayubi <sample@madeup.com>
2024-04-18 09:55:04 +0100

    Deleted in memory dict as database and related code

--- app/main.py
+++ app/main.py
@@ -9,32 +9,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-my_posts = [
-    {
-        "title": "title of post 1",
-        "content": "content of post 1",
-        "id": 1
-    },
-    {
-        "title": "favourite foods",
-        "content": "I like pizza",
-        "id": 2
-    }
-]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-        
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
