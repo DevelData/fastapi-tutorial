914f18bad8398c2e116ee9a6a15ffe7950e52cf4
Areeb Ayubi <sample@madeup.com>
2024-04-18 09:56:52 +0100

    Formatting changes

--- app/main.py
+++ app/main.py
@@ -7,8 +7,6 @@ from app.routers import auth, post, user
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
