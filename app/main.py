0bc9a18f322e6c2adf8923830755b4fd0cc1c4f4
Areeb Ayubi <sample@madeup.com>
2024-04-20 04:17:12 +0100

    Commented out command to generate tables from SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app import models
 from app.database import engine
 from app.routers import auth, post, user, vote
 
 
+# from app import models
+# models.Base.metadata.create_all(bind=engine)
 
-models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
