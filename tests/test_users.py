ed9dbb4d20c6dfb040421913034c8c0b03315986
Areeb Ayubi <sample@madeup.com>
2024-04-23 16:08:15 +0100

    Added test_create_user for testing creating users

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,23 @@
-from fastapi.testclient import TestClient
-from app.main import app
+from fastapi import status
+from app import schemas
+from tests.database import client, session
 
 
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to my first API"}
+
+
+def test_create_user(client):
+    response = client.post(
+        "/users/",
+        json={
+            "email": "hello123@gmail.com",
+            "password": "password123"
+            })
+    
+    new_user = schemas.UserOut(**response.json())
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert new_user.email == "hello123@gmail.com"
