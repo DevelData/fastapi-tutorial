2de4728e532eb0e06e284fe3ced5157cfeaf2865
Areeb Ayubi <sample@madeup.com>
2024-04-26 16:03:17 +0100

    Added test_get_all_posts test in test_posts.py

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,6 +1,10 @@
 from fastapi import status
 
 
-def test_get_all_posts(authorized_client):
+
+def test_get_all_posts(authorized_client, test_posts):
     response = authorized_client.get(url="/posts/")
+    #validate = lambda x: schemas.PostOut(**x)
+    #posts = list(map(validate, response.json()))
     assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == len(test_posts)
