4a578e0744252a3a5c4c75b80d038ad51e2b521c
Areeb Ayubi <sample@madeup.com>
2024-04-26 20:17:03 +0100

    Added test_create_post in test_posts.py

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,5 @@
 from fastapi import status
+import pytest
 from app import schemas
 
 
@@ -33,3 +34,25 @@ def test_get_one_post(authorized_client, test_posts):
     assert post.Post.id == test_posts[0].id
     assert post.Post.title == test_posts[0].title
     assert post.Post.content == test_posts[0].content
+
+
+@pytest.mark.parametrize("title, content, published", [
+    ("awesome new title", "awesome new content", True),
+    ("favorite pizza", "i love pepperoni", False),
+    ("tallest skyscrapers", "wahoo", True)
+])
+def test_create_post(authorized_client, test_user, title, content, published):
+    response = authorized_client.post(
+        url="/posts/",
+        json={
+            "title": title,
+            "content": content,
+            "published": published
+            }
+        )
+    created_post = schemas.Post(**response.json())
+    assert response.status_code == status.HTTP_201_CREATED
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
+    assert created_post.owner_id == test_user["id"]
